485a828ee26901b415751688d8639f9ad23bca17
Andrew Bychinski <aebychinski@live.com>
2022-11-02 19:58:31 -0500

    .

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,7 +6,7 @@
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
 
-
+        //entity framework navigation properties
         public virtual ICollection<MovieGenre> MovieGenres { get; set; }
         public virtual ICollection<UserMovie> UserMovies { get; set; }
     }
